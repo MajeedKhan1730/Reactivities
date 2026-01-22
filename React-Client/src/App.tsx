64edc21838eb24bec442a3fc744a9d24199e7822
Majeed Khan <majeedkhan.pk@hotmail.com>
2026-01-22 23:32:42 +0400

    React Hooks and API Data Fetch

--- React-Client/src/App.tsx
+++ React-Client/src/App.tsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const title = "Wolcome to Reactivities";
+  const [activities, setActivities] = useState([]);
+  useEffect(() => {
+    fetch('https://localhost:7028/api/activities')
+      .then(response => response.json())
+      .then(data => setActivities(data))
+  }, [])
   return (
-    <h1>Reactivities</h1>
+    <div>
+          <h1 className="app" style={{color:'red'}}>{title}</h1>
+    <ul>
+      {activities.map((activity)=>(
+        <li key={activity.id}>{activity.title}</li>
+      ))}
+    </ul>
+    </div>
+
   )
 }
 
